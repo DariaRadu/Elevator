f94983e9c63e32da9411d335e891192963ff6544
Daria Radu <dariaradur3@gmail.com>
2020-04-01 00:28:14 +0200

    Add introductory message

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ let numberOfPeople = 0;
 promptQuestion = async () => {
 
     floors = [];
+    io.write(`Welcome to the elevator! This block has ${elevator.maxFloor} floors and you are currently at ${elevator.currentFloor}.`);
     numberOfPeople = parseInt(await io.ask(`How many people are entering?`));
     for(let i = 0; i < numberOfPeople; i++) {
         floors.push(parseInt(await io.ask(`What floor is person ${i+1} going to?`)))
