bcaaad5616922ffc8aedc55e6323b47b6e970811
Daria Radu <dariaradur3@gmail.com>
2020-04-01 00:29:09 +0200

    Add missing colon

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ promptQuestion = async () => {
     io.write(`Welcome to the elevator! This block has ${elevator.maxFloor} floors and you are currently at ${elevator.currentFloor}.`);
     numberOfPeople = parseInt(await io.ask(`How many people are entering?`));
     for(let i = 0; i < numberOfPeople; i++) {
-        floors.push(parseInt(await io.ask(`What floor is person ${i+1} going to?`)))
+        floors.push(parseInt(await io.ask(`What floor is person ${i+1} going to?`)));
     }
     
     await elevator.goToFloors(floors);
